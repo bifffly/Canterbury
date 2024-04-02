b024f8ed6d4c9c9b2afb9cd8ea45498b092ab763
Bryce Colson <bryzcolson@gmail.com>
2024-04-02 18:00:53 -0400

    Added debug printing

--- c/src/main.c
+++ c/src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#include "chunk.h"
+#include "debug.h"
+
 int main() {
-    printf("Hello, World!\n");
+    Chunk chunk;
+
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RET);
+    disassembleChunk(&chunk, "test chunk");
+
+    freeChunk(&chunk);
     return 0;
 }
